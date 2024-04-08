d1cdec8b7fdd2be8433bcc98136380769c707d49
hahongha <phamha03122003@gmail.com>
2024-04-08 14:01:46 +0700

    test

--- src/control/Mouse.java
+++ src/control/Mouse.java
@@ -1,13 +1,82 @@
 package control;
 
+import main.Main;
+import model.Board;
+import view.MainPanel;
+import view.startScreen;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Mouse extends MouseAdapter {
     public int mx, my;
     public boolean pressed = false;
+
+    public startScreen startScreen;
+
+    public Board board;
+    public MainPanel gamePanel;
+
+    public Main main;
+
+    public Mouse(Board board1, MainPanel game, Main jf){
+        main = jf;
+        board= board1;
+        gamePanel = game;
+       // startScreen = new startScreen(main);
+    }
     @Override
     public void mouseClicked(MouseEvent e) {//nhan tha
+        mx= e.getX();
+        my= e.getY();
+        if(board.isGameOn()&& board.isGameContinue()){
+            if(mx> MainPanel.BOARD_WIDTH+80 && mx<MainPanel.BOARD_WIDTH+80+220){
+                //pause
+                if(my>400&& my<470){
+                    //JOptionPane.showMessageDialog(null, "Click pause");
+                    if(board.isBlack||board.isWhite) {
+                        board.isBlack = false;
+                        board.isWhite = false;
+                    }else{
+                        if(!board.WhiteContinue) board.isBlack= true;
+                        else board.isWhite= true;
+                    }
+                    board.setGameContinue(false);
+                }
+                //exit
+                if(my>500&&my<570){
+                    //gamePanel.addAncestorListener();
+                    board.setGameOn(false);
+                }
+            }
+        } else if (!board.isGameOn()) {
+            if(mx>400&&mx<700){
+                if (my>400&&my<500)
+                    startScreen = new startScreen(main);
+                    main.movePanels(gamePanel, startScreen);
+            }
+        } else if (!board.isGameContinue()) {
+            if(mx>400&&mx<700){
+                //exit
+                if (my>520&&my<600)
+                    //JOptionPane.showMessageDialog(null, "Click exit");
+                    startScreen = new startScreen(main);
+                    main.movePanels(gamePanel, startScreen);
+                //replay
+                if (my>390&&my<470) {
+                    board.reset();
+                    gamePanel.timeWhite.setStartTime(System.nanoTime());
+                    gamePanel.timeBlack.setStartTime(System.nanoTime());
+                }
+                //resume
+                if (my>260&&my<330)
+                    board.setGameContinue(true);
+            }
+        }
+
+
     }
 
     @Override
@@ -15,6 +84,7 @@ public class Mouse extends MouseAdapter {
         pressed= true;
         mx= e.getX();
         my= e.getY();
+
     }
 
     @Override
--- src/model/Board.java
+++ src/model/Board.java
@@ -2,198 +2,301 @@ package model;
 
 public class Board {
     protected Piece[][] board;
+    protected Piece[][] dudoanboard;
     public static final int ROWS = 8;
     public static final int COLS = 8;
+    private int scoreWhite;
+    private int scoreBlack;
+    private int i,j=0;
+
+    public boolean isBlack = true;//hien tai dang la quan den
+    public boolean isWhite=false;//hien tai dang la quan den
+
+    public boolean WhiteContinue= false;
+
+    private boolean gameOn;
+
+    private boolean gameContinue;
 
     public Board(){
         board= new Piece[ROWS][COLS];
+        gameOn= true;
+        gameContinue = true;
         startBoard();
-
     }
 
     private void startBoard(){
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
+        scoreBlack=2;
+        scoreWhite=2;
+        for (i = 0; i < ROWS; i++) {
+            for (j = 0; j < COLS; j++) {
                     Piece piece = new Piece(i, j, 0);
                     board[i][j] = piece;
             }
         }
-        printBoard();
+
         board[ROWS/2-1][COLS/2-1].setValue(2);
-        board[3][6].setValue(1);
         board[ROWS/2][COLS/2].setValue(2);
         board[ROWS/2][COLS/2-1].setValue(1);
         board[ROWS/2-1][COLS/2].setValue(1);
+
     }
 
-    public void printBoard(){
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                System.out.print(board[i][j].getValue()+" ");
+    public void update(int row, int col) {
+        gameContinue = setDead();
+        if(gameContinue){
+
+            if(isBlack && !isWhite) {
+                Piece newPiece = new Piece(row, col, 1);
+                if (canPutPiece(row, col, 1)) {
+                    board[row][col] = newPiece;
+                    System.out.println("black");
+                }else System.out.println("black is fail");
+
+                isWhite= true;
+                isBlack= false;
+                WhiteContinue=true;
             }
-            System.out.println();
+            else if(!isBlack && isWhite) {
+                Piece newPiece = new Piece(row, col, 2);
+                if (canPutPiece(row, col, 2)) {
+                    board[row][col] = newPiece;
+                    System.out.println("white");
+                }else System.out.println("white is fail");
+                isWhite= false;
+                isBlack= true;
+                WhiteContinue= false;
+            }
         }
-        System.out.println("----------------------");
     }
+
+    public int getScore(int value){
+        int score=0;
+        for (i = 0; i < Board.ROWS; i++) {
+            for (j = 0; j < Board.COLS; j++) {
+                if(board[i][j].getValue()==value) score+=1;
+            }
+        }
+        return score;
+    }
+
+    //tinh so quan trang trong moi lan lat sau do su dung minimax tinh maxWhite moi lan quan trang chay va minWhite moi lan quan dem=n chay
+    //chay het ban co return ra cot va hang co gia tri score tang len thi duoc phep chay
+    //su dung 1 map (list) de luu tru hang va cot co the chay
+    //phat trien theo huong lua chon max
 
     public Piece[][] getBoard() {
         return board;
     }
-    //kiem tra xem co di chuyen duoc khong
-    public boolean isValidMove(int row, int col) {
-        // Check if the cell is empty and within the bounds of the board
-        if(row>=Board.ROWS ||col>= Board.COLS|| row<0||col<0)
-            return false;
-        return true;
-    }
     //kiem tra xem co dat co duoc khong
-    public boolean canPutPiece(int col, int row, int value){
-        //col
-        //xet sang trai
-        for (int i = col-1; i >=0 ; i--) {
-            if(board[row][i].getValue()==0)
+    public boolean canPutPiece(int row, int col, int value){
+        boolean canMove = false;
+        if(board[row][col].getValue() !=0) return false;
+        //xet huong len tren xem co quan nao lat duoc khong
+        for (i = row-1; i >=0 ; i--) {
+            if(board[i][col].getValue()==0){
+                canMove= false;
                 break;
-            if(board[row][i].getValue()==value){
-                return true;
             }
-        }
-//        //xét sang phai
-        for (int i = col+1; i <Board.COLS ; i++) {
-            if(board[row][i].getValue()==0)
-                break;
-            if(board[row][i].getValue()==value){
-                return true;
-            }
-        }
-//
-//        //row
-//        //xet len
-        for (int i = row-1; i >=0 ; i--) {
-            if(board[i][col].getValue()==0)
-                break;
             if(board[i][col].getValue()==value){
-                return true;
-            }
-        }
-        //xet xuong
-        for (int i = row+1; i < Board.ROWS ; i++) {
-            if(board[i][col].getValue()==0)
+                flipPieceTop(row,col,value);
+                canMove=true;
                 break;
+            }
+        }
+        //xet huong xuong duoi xem co quan co nao lat duoc khong
+        for (i = row+1; i <Board.ROWS ; i++) {
+            if(board[i][col].getValue()==0){
+                canMove= false;
+                break;
+            }
             if(board[i][col].getValue()==value){
-                return true;
+                flipPieceBottom(row,col,value);
+                canMove=true;
+                break;
             }
         }
-        //xet duoi phai
-        for (int i = row+1; i < Board.ROWS ; i++) {
-            for (int j = col+1; j < Board.COLS ; j++) {
-                if(board[i][j].getValue()==0)
+        //xet huong sang phai
+        for (i = col+1; i <Board.COLS ; i++) {
+            if(board[row][i].getValue()==0){
+                canMove= false;
+                break;
+            }
+            if(board[row][i].getValue()==value){
+                flipPieceRight(row,col,value);
+                canMove=true;
+                break;
+            }
+        }
+        //xet huong sang trai
+        for (i = col-1; i >=0 ; i--) {
+            if(board[row][i].getValue()==0){
+                canMove= false;
+                break;
+            }
+            if(board[row][i].getValue()==value){
+                flipPieceLeft(row,col,value);
+                canMove=true;
+                break;
+            }
+        }
+        //xet huong cheo tren ben trai
+        for (i = row-1, j = col-1; i >=0 && j>=0 ; i--, j--){
+            if (board[i][j].getValue() == 0) {
+                break;
+            }
+            if (board[i][j].getValue() == value) {
+                flipPieceTopLeft(row, col, value);
+                canMove = true;
+            }
+        }
+
+//        //xet huong cheo tren phai
+        for (i = row-1, j= col+1; i >=0&&j < Board.COLS ; i--,j++) {
+                if(board[i][j].getValue()==0){
                     break;
-                if(board[i][j].getValue()==value){
-                    return true;
                 }
-            }
+                if(board[i][j].getValue()==value){
+                    flipPieceTopRight(row,col,value);
+                    canMove=true;
+                }
+
         }
-        //xet tren trai
-        for (int i = row-1; i >=0 ; i--) {
-            for (int j = col-1; j >=0 ; j--) {
-                if(board[i][j].getValue()==0)
+////
+////        //xet huong cheo duoi trai
+        for (i = row+1, j= col-1; i <Board.ROWS&&j>=0 ; i++,j--) {
+                if (board[i][j].getValue() == 0) {
                     break;
-                if(board[i][j].getValue()==value){
-                    return true;
                 }
-            }
+                if (board[i][j].getValue() == value) {
+                    flipPieceBottomLeft(row, col, value);
+                    canMove=true;
+                }
         }
-        //xet tren phai
-        for (int i = row-1; i >=0 ; i--) {
-            for (int j = col+1; j <Board.COLS ; j++) {
-                if(board[i][j].getValue()==0)
+////
+////        //xet duoi phai
+        for (i = row+1,j = col + 1; i <Board.ROWS && j < Board.COLS ; i++) {
+                if (board[i][j].getValue() == 0) {
                     break;
-                if(board[i][j].getValue()==value){
-                    return true;
                 }
-            }
+                if (board[i][j].getValue() == value) {
+                    flipPieceBottomRight(row, col, value);
+                    canMove=true;
+                }
         }
-        //xet duoi phai
-        for (int i = row+1; i < Board.ROWS ; i++) {
-            for (int j = col-1; j >=0 ; j--) {
-                if(board[i][j].getValue()==0)
+        return canMove;
+    }
+
+
+    public void flipPieceTop(int row, int col, int value){
+        for (int i = row; i >=0 ; i--) {
+            if(board[i][col].getValue()==value) {
+                break;
+            }else board[i][col].setValue(value);
+        }
+    }
+    public void flipPieceBottom(int row, int col, int value){
+        for (int i = row; i <Board.ROWS ; i++) {
+            if(board[i][col].getValue()==value) {
+                break;
+            }else board[i][col].setValue(value);
+        }
+    }
+    public void flipPieceBottomLeft(int row, int col, int value){
+        for (i = row, j= col; i <Board.ROWS&&j>=0 ; i++,j--){
+            if(board[i][j].getValue()==value) {
+                break;
+            }else board[i][j].setValue(value);
+    }
+    }
+
+    public void flipPieceBottomRight(int row, int col, int value) {
+        for (i = row,j = col; i <Board.ROWS && j < Board.COLS ; i++) {
+                if (board[i][j].getValue() == value) {
                     break;
-                if(board[i][j].getValue()==value){
-                    return true;
-                }
+                } else board[i][j].setValue(value);
+        }
+    }
+    public void flipPieceTopRight(int row, int col, int value) {
+        int i,j;
+        for (i = row, j= col; i >=0 &&j < Board.COLS ; i--,j++){
+
+                if (board[i][j].getValue() == value) {
+                    break;
+                } else board[i][j].setValue(value);
+        }
+    }
+
+    public void flipPieceTopLeft(int row, int col, int value){
+        int i,j;
+        for (i = row, j = col; i >=0 && j>=0 ; i--, j--){
+                if (board[i][j].getValue() == value) {
+                    break;
+                } else board[i][j].setValue(value);
+        }
+    }
+
+    public void flipPieceRight(int row, int col, int value){
+        for (int i = col; i <Board.COLS ; i++) {
+            if(board[row][i].getValue()==value) {
+                break;
+            }else board[row][i].setValue(value);
+        }
+    }
+    public void flipPieceLeft(int row, int col, int value){
+        for (int i = col; i >=0 ; i--) {
+            if(board[row][i].getValue()==value) {
+                break;
+            }else board[row][i].setValue(value);
+        }
+    }
+
+    private boolean setDead(){
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLS; j++) {
+                if(board[i][j].getValue() ==0) return true;
             }
         }
-        printBoard();
         return false;
     }
-//lat co
-    public void flipPiece(int col, int row, int value){
-        //col
-        //xet sang trai
-        for (int i = col-1; i >=0 ; i--) {
-            if(board[row][i].getValue()==value){
-                for (int j = col-1; j >i ; j--) {
-                    board[row][j].setValue(value);
-                }
-                break;
-            }
-        }
-//        //xét sang phai
-        for (int i = col+1; i <Board.COLS ; i++) {
-            //khong tim duoc o de ket noi
-            if (board[row][i].getValue()==0){
-                break;
-            }
-            if(board[row][i].getValue()==value){
-                for (int j = col+1; j <i ; j++) {
-                    board[row][j].setValue(value);
-                }
-                break;
-            }
-        }
 
-        //        //xet len
-        for (int i = row-1; i >=0 ; i--) {
-            //khong tim duoc o de ket noi
-            if (board[i][col].getValue()==0){
-                break;
-            }
-            if(board[i][col].getValue()==value){
-                for (int j = row-1; j >i ; j--) {
-                    board[j][col].setValue(value);
-                }
-                break;
-            }
-        }
-        //xet xuong
-        for (int i = row+1; i < Board.ROWS ; i++) {
-            //khong tim duoc o de ket noi
-            if (board[i][col].getValue()==0){
-                break;
-            }
-            if(board[i][col].getValue()==value){
-                for (int j = row+1; j <i ; j++) {
-                    board[j][col].setValue(value);
-                }
-                break;
-            }
-        }
-        //xet tren trai
-        for (int i = row-1; i >=0 ; i--) {
-            for (int j = col-1; j >=0 ; j--) {
-                if(board[i][j].getValue()==value||board[i][j].getValue()==0){
-                    break;
-                }
-                else{
-                    board[i][j].setValue(value);
-                }
-            }
-        }
+    public void reset(){
+        board= new Piece[ROWS][COLS];
+        gameOn= true;
+        gameContinue = true;
+        startBoard();
 
-        printBoard();
     }
+
+    public void printBoard(){
+        for (int k = 0; k < Board.COLS; k++) {
+            for (int l = 0; l < Board.ROWS; l++) {
+                System.out.print("" + board[k][l].getValue()+" ");
+            }
+            System.out.println();
+        }
+    }
+
 
     public void setBoard(Piece[][] board) {
         this.board = board;
+    }
+    public int getScoreWhite(){return scoreWhite;}
+    public int getScoreBlack(){return scoreBlack;}
+
+    public boolean isGameOn() {
+        return gameOn;
+    }
+
+    public void setGameOn(boolean gameOn) {
+        this.gameOn = gameOn;
+    }
+
+    public boolean isGameContinue() {
+        return gameContinue;
+    }
+
+    public void setGameContinue(boolean gameContinue) {
+        this.gameContinue = gameContinue;
     }
 }
